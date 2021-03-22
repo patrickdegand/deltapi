37ce901c856fea80e964b0266f3d1995997e4b24
degand <pdegand@mailinator.com>
2021-03-22 10:31:04 +0100

    create github pages

---
+++ assets/deltapi/deltapi.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function(){
+	$('[data-email]').on('click', function () {
+		var base64email =$(this).attr("data-email");
+		window.location.href = `mailto:${atob(base64email)}`;
+	});
+	$("form").submit(function(){
+		var fileUrl =$(this).attr("data-file");
+		window.location = atob(fileUrl);
+	});
+});
+function mailtodeltapi(){
+	var dataemail="ZGVsdGFfcGlAb3V0bG9vay5mcg=="
+	window.location.href = `mailto:${atob(dataemail)}`;
+}
+!function(){
+	if(document.getElementById('top-1')){
+	var element = document.getElementById('top-1');
+	element.parentNode.removeChild(element);
+}}();

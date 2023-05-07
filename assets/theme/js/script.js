05f8d83347b028ab032f4b2194fd776625425941
degand <pdegand@mailinator.com>
2023-05-07 08:04:18 +0200

    create github pages

--- assets/theme/js/script.js
+++ assets/theme/js/script.js
@@ -1,4 +1,4 @@
-!function(){try{document.getElementsById("top-1")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.style="display: none";a.innerHTML='<a href="https://mobirise.com/builder/web-design-program.html">website design program</a> Mobirise v5.8.9 <a href="https://mobirise.com/extensions/popup-builder/">popup window</a>';document.body.insertBefore(a,document.body.childNodes[0])}}();
+!function(){try{document.getElementsById("top-1")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.style="display: none";a.innerHTML='<a href="https://mobirise.com/builder/landing-page-builder.html">landing page Maker</a> Mobirise v5.8.9 <a href="https://mobirise.com/builder/easiest-website-builder.html">easiest website builder for beginners</a>';document.body.insertBefore(a,document.body.childNodes[0])}}();
 //alert("W3CSS theme");
 	$('.jqte-test').jqte();
 	

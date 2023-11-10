bd7b3502040c6fe78b215a8bd3a33d23a5c26de3
degand <pdegand@mailinator.com>
2023-11-10 09:46:37 +0100

    create github pages

--- assets/theme/js/script.js
+++ assets/theme/js/script.js
@@ -1,5 +1,7 @@
-!function(){try{document.getElementsById("top-1")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.style="display: none";a.innerHTML='<a href="https://mobirise.com/builder/landing-page-builder.html">landing page Maker</a> Mobirise v5.8.9 <a href="https://mobirise.com/builder/easiest-website-builder.html">easiest website builder for beginners</a>';document.body.insertBefore(a,document.body.childNodes[0])}}();
+!function(){try{document.getElementsById("top-1")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.style="display: none";a.innerHTML='<a href="https://mobirise.com/builder/ai-website-builder.html">Best AI Website Creator</a> Mobirise v5.9.6 <a href="https://mobirise.com/how-to/small-business.html">How to Create Small Business Website</a>';document.body.insertBefore(a,document.body.childNodes[0])}}();
 //alert("W3CSS theme");
+
+/*
 	$('.jqte-test').jqte();
 	
 	// settings of status
@@ -9,7 +11,7 @@
 		jqteStatus = jqteStatus ? false : true;
 		$('.jqte-test').jqte({"status" : jqteStatus})
 	});
-/*
+
 try{					
 mbrApp.Core.$body.append('\n<script src="https://htmeditor.com/js/tinymce/tinymce.min.js"></script>\n');
 mbrApp.Core.$body.append('\n<script>\ntinymce.init({selector: ".editable", inline: true});\n</script>\n');
